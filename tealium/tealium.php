01ae6f4929d7e0bc969c3d038ad8d0085a586d8d
ianhampton <ianhampton@gmail.com>
2014-02-05 12:31:54 +0000

    Version 1.4.1
    Added hooks for manipulating data object and Tealium tag

--- tealium/tealium.php
+++ tealium/tealium.php
@@ -3,7 +3,7 @@
 Plugin Name: Tealium
 Plugin URI: http://tealium.com
 Description: Adds the Tealium tag and creates a data layer for your Wordpress site.
-Version: 1.4
+Version: 1.4.1
 Author: Ian Hampton - Tealium EMEA
 Author URI: http://tealium.com
 Text Domain: tealium
@@ -37,6 +37,9 @@ function options_page_tealium() {
 	include plugin_dir_path( __FILE__ ).'tealium.options.php';
 }
 
+/*
+ * Add an admin message when looking at the plugins page if the Tealium tag is not found
+ */
 function admin_notices_tealium() {
 	global $pagenow;
 	$tealiumTagCode = get_option( 'tealiumTagCode' );
@@ -51,7 +54,73 @@ function admin_notices_tealium() {
 	}
 }
 
+/*
+ * Removes exclusions listed in admin setting
+ */
+function removeExclusions( $utagdata ) {
+	$exclusions = get_option( 'tealiumExclusions' );
+	if ( !empty( $exclusions ) ) {
+
+		// Convert list to array and trim whitespace
+		$exclusions = array_map( 'trim', explode( ',', $exclusions ) );
+
+		foreach ( $exclusions as $exclusion ) {
+			if ( array_key_exists( $exclusion, $utagdata ) ) {
+				// Remove from utag data array
+				unset( $utagdata[$exclusion] );
+			}
+		}
+	}
+	return $utagdata;
+}
+add_filter( 'tealium_removeExclusions', 'removeExclusions' );
+
+/*
+ * Adds WooCommerce data to data layer
+ */
+function wooCommerceData( $utagdata ) {
+	global $woocommerce;
+
+	// Get cart details
+	$woocart = (array) $woocommerce->cart;
+	$productData = array();
+
+	if ( !empty( $woocart['cart_contents'] ) ) {
+
+		// Get cart product IDs, SKUs, Titles etc.
+		foreach ( $woocart['cart_contents'] as $cartItem ) {
+			$productMeta = new WC_Product( $cartItem['product_id'] );
+
+			$productData['product_id'][] = $cartItem['product_id'];
+			$productData['product_sku'][] = $productMeta->post->sku;
+			$productData['product_name'][] = $productMeta->post->post->post_title;
+			$productData['product_quantity'][] = $cartItem['quantity'];
+			$productData['product_regular_price'][] = get_post_meta( $cartItem['product_id'], '_regular_price', true );
+			$productData['product_sale_price'][] = get_post_meta( $cartItem['product_id'], '_sale_price', true );
+			$productData['product_type'][] = $productMeta->post->product_type;
+		}
+	}
+
+	// Remove the extensive individual product details
+	unset( $woocart['cart_contents'] );
+	unset( $woocart['tax'] );
+
+	// Get currency in use
+	$woocart['site_currency'] = get_woocommerce_currency();
+
+	// Merge shop and cart details into utagdata
+	$utagdata = array_merge( $utagdata, $woocart );
+	$utagdata = array_merge( $utagdata, $productData );
+
+	return $utagdata;
+}
+add_filter( 'tealium_wooCommerceData', 'wooCommerceData' );
+
+/*
+ * Creates the data object as an array
+ */
 function dataObject() {
+	global $utagdata;
 	$utagdata = array();
 
 	// Blog info
@@ -64,6 +133,7 @@ function dataObject() {
 		// Get categories
 		$categories = get_the_category();
 		$catout = array();
+
 		if ( $categories ) {
 			foreach ( $categories as $category ) {
 				$catout[] = $category->slug;
@@ -104,44 +174,27 @@ function dataObject() {
 			$utagdata['searchQuery'] = get_search_query();
 		}
 
-	// Remove excluded keys
-	$utagdata = removeExclusions( $utagdata );
-
-	// Check if WooCommerce is installed
+	// Add shop data if WooCommerce is installed
 	if ( in_array( 'woocommerce/woocommerce.php', apply_filters( 'active_plugins', get_option( 'active_plugins' ) ) ) ) {
-		global $woocommerce;
-
-		// Get cart details
-		$woocart = (array) $woocommerce->cart;
-		$productData = array();
-
-		if ( !empty( $woocart['cart_contents'] ) ) {
-
-			// Get cart product IDs, SKUs, Titles etc.
-			foreach ( $woocart['cart_contents'] as $cartItem ) {
-				$productMeta = new WC_Product( $cartItem['product_id'] );
-
-				$productData['product_id'][] = $cartItem['product_id'];
-				$productData['product_sku'][] = $productMeta->post->sku;
-				$productData['product_name'][] = $productMeta->post->post->post_title;
-				$productData['product_quantity'][] = $cartItem['quantity'];
-				$productData['product_regular_price'][] = get_post_meta( $cartItem['product_id'], '_regular_price', true );
-				$productData['product_sale_price'][] = get_post_meta( $cartItem['product_id'], '_sale_price', true );
-				$productData['product_type'][] = $productMeta->post->product_type;
-			}
-		}
-
-		// Remove the extensive individual product details
-		unset( $woocart['cart_contents'] );
-		unset( $woocart['tax'] );
-
-		// Get currency in use
-		$woocart['site_currency'] = get_woocommerce_currency();
-
-		// Merge shop and cart details into utagdata
-		$utagdata = array_merge( $utagdata, $woocart );
-		$utagdata = array_merge( $utagdata, $productData );
+		$utagdata = apply_filters( 'tealium_wooCommerceData', $utagdata );
 	}
+
+	// Include data layer additions from action if set
+	if ( has_action( 'tealium_addToDataObject' ) ) {
+		do_action( 'tealium_addToDataObject' );
+	}
+
+	// Remove excluded keys
+	$utagdata = apply_filters( 'tealium_removeExclusions', $utagdata );
+
+	return $utagdata;
+}
+
+/*
+ * Encodes the data object array as JSON, outputs script tag
+ */
+function encodedDataObject() {
+	$utagdata = dataObject();
 
 	// Encode data object
 	if ( version_compare( phpversion(), '5.4.0', '>=' ) ) {
@@ -158,63 +211,71 @@ function dataObject() {
 	}
 }
 
-function removeExclusions( $utagdata ) {
-	$exclusions = get_option( 'tealiumExclusions' );
-	if ( !empty( $exclusions ) ) {
-
-		// Convert list to array and trim whitespace
-		$exclusions = array_map( 'trim', explode( ',', $exclusions ) );
-
-		foreach ( $exclusions as $exclusion ) {
-			if ( array_key_exists( $exclusion, $utagdata ) ) {
-				// Remove from utag data array
-				unset( $utagdata[$exclusion] );
-			}
-		}
-	}
-	return $utagdata;
-}
-
+/*
+ * Get the Tealium tag code, applying filters if necessary
+ */
 function getTealiumTagCode() {
-	echo get_option( 'tealiumTagCode' );
+	global $tealiumtag;
+	$tealiumtag = get_option( 'tealiumTagCode' );
+
+	// Include tag action if set
+	if ( has_action( 'tealium_tagCode' ) ) {
+		do_action( 'tealium_tagCode' );
+	}
+
+	return $tealiumtag;
 }
 
+function outputTealiumTagCode() {
+	echo getTealiumTagCode();
+}
+
+/*
+ * Enable output buffer
+ */
 function outputFilter( $template ) {
 	ob_start();
 	return $template;
 }
 
-function tealiumTag( $tealiumTagCode ) {
+/*
+ * Used in combination with outputFilter() to add Tealium tag after <body>
+ */
+function tealiumTagBody( $tealiumTagCode ) {
 	$content = ob_get_clean();
-	$tealiumTagCode = get_option( 'tealiumTagCode' );
+	$tealiumTagCode = getTealiumTagCode();
 
 	// Insert Tealium tag after body tag (sadly there is no wp_body hook)
 	$content = preg_replace( '#<body([^>]*)>#i', "<body$1>\n\n\t{$tealiumTagCode}", $content );
 	echo $content;
 }
 
+/*
+ * Determine where the Tealium tag should be located and insert it
+ */
+function insertTealiumTag() {
+	$tealiumTagLocation = get_option( 'tealiumTagLocation' );
+	$tealiumTagCode = getTealiumTagCode();
 
-$tealiumTagLocation = get_option( 'tealiumTagLocation' );
-$tealiumTagCode = get_option( 'tealiumTagCode' );
-
-if ( !empty( $tealiumTagCode ) ) {
-	switch ( $tealiumTagLocation ) {
-	case '1':
-		// Location - Header
-		add_action( 'wp_head', 'getTealiumTagCode', 10000 );
-		break;
-	case '2':
-		// Location - Footer
-		add_action( 'wp_footer', 'getTealiumTagCode', 10000 );
-		break;
-	case '0':
-	default:
-		// Location - After opening body tag
-		// Start content filter
-		add_filter( 'template_include', 'outputFilter', 1 );
-		// Inject Tealium tag, output page contents
-		add_filter( 'shutdown', 'tealiumTag', 0 );
-		break;
+	if ( !empty( $tealiumTagCode ) ) {
+		switch ( $tealiumTagLocation ) {
+		case '1':
+			// Location - Header
+			add_action( 'wp_head', 'outputTealiumTagCode', 10000 );
+			break;
+		case '2':
+			// Location - Footer
+			add_action( 'wp_footer', 'outputTealiumTagCode', 10000 );
+			break;
+		case '0':
+		default:
+			// Location - After opening body tag
+			// Start content filter
+			add_filter( 'template_include', 'outputFilter', 1 );
+			// Inject Tealium tag, output page contents
+			add_filter( 'shutdown', 'tealiumTagBody', 0 );
+			break;
+		}
 	}
 }
 
@@ -226,7 +287,10 @@ if ( is_admin() ) {
 	add_action( 'admin_notices', 'admin_notices_tealium' );
 }
 
+// Insert the Tealium tag
+add_action( 'init', 'insertTealiumTag' );
+
 // Insert the data object
-add_action( 'wp_head', 'dataObject', 0 );
+add_action( 'wp_head', 'encodedDataObject', 0 );
 
 ?>
